33616559eec709b310f4bf431cac1c8a034f8202
nhamhv <hoangnham01@gmail.com>
2019-05-13 08:03:37 +0700

    Add scoped-models

---
+++ lib/scoped-models/products.dart
@@ -0,0 +1,59 @@
+import 'package:scoped_model/scoped_model.dart';
+
+import '../models/product.dart';
+
+class ProductsModel extends Model {
+  List<Product> _products = [];
+  int _selectedProductIndex;
+
+  List<Product> get products {
+    return List.from(_products);
+  }
+
+  int get selectedProductIndex {
+    return _selectedProductIndex;
+  }
+
+  Product get selectedProduct {
+    if (_selectedProductIndex == null) {
+      return null;
+    }
+
+    return _products[_selectedProductIndex];
+  }
+
+  void addProduct(Product product) {
+    _products.add(product);
+    _selectedProductIndex = null;
+    notifyListeners();
+  }
+
+  void updateProduct(Product product) {
+    _products[_selectedProductIndex] = product;
+    _selectedProductIndex = null;
+    notifyListeners();
+  }
+
+  void deleteProduct() {
+    _products.removeAt(_selectedProductIndex);
+    _selectedProductIndex = null;
+    notifyListeners();
+  }
+
+  void toggleProductFavoriteStatus() {
+    final isCurrentlyFavorite = products[_selectedProductIndex].isFavorite;
+    final Product productUpdate = Product(
+      title: selectedProduct.title,
+      description: selectedProduct.description,
+      price: selectedProduct.price,
+      image: selectedProduct.image,
+      isFavorite: !isCurrentlyFavorite,
+    );
+
+    updateProduct(productUpdate);
+  }
+
+  void selectProduct(int index) {
+    _selectedProductIndex = index;
+  }
+}
